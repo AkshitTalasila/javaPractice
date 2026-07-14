3f524cf9906cf5a21d5f0b6cdeec5b20b4fb1efd
Akshit Talasila <akshittalasila@Anitas-MacBook-Pro-2.local>
2026-07-14 13:50:51 -0400

    Commit

--- javaPractice/ClassesAndObjects/Floor.java
+++ javaPractice/ClassesAndObjects/Floor.java
@@ -2,4 +2,32 @@ public class Floor{
 
 	private double width;
 	private double length;
+
+	public Floor(double width, double length){
+
+		if((width<0)&&(length<0)){
+		
+			this.width = 0;
+			this.length = 0;
+		}else if(width<0){
+		
+			this.width =0;
+			this.length = length;
+		}else if(length<0){
+
+			this.length =0;
+			this.width = width;
+		}else {
+
+			this.length = length;
+			this.width = width;
+		}
+	}
+
+
+
+	public double getArea(){
+
+		return width*length;
+	}
 }
